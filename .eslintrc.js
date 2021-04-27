e08582048f72e69c2ae99a6a44d0ede9c20c2953
Rintaro Hasegawa <rintaro.hasegawa@gmail.com>
2021-04-27 18:38:28 +0900

    config: update eslint for nextjs

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,9 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [2, { extensions: ['.tsx'] }],
+
   },
 };
