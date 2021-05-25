c3565aeee4c928569308dc7082935d696b25ebf0
Rintaro Hasegawa <rintaro.hasegawa@gmail.com>
2021-05-25 18:02:03 +0900

    feature: add basic layout

---
+++ pages/Layout.tsx
@@ -0,0 +1,7 @@
+export default function Layout({ children }) {
+  return (
+    <div className="bg-green-50">
+      {children}
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
+import Layout from './Layout';
+
 export default function Home() {
   return (
-    <div className="font-bold">
-      IZA Template
-    </div>
+    <Layout>
+      <div className="flex">
+        <div className="my-4 mx-auto font-bold text-green-900 tracking-widest text-xl">
+          こんにちわ！新海農園です！
+        </div>
+      </div>
+    </Layout>
   );
 }
