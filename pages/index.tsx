aeaf19e6d359870364c20c5014a65bf78429dcf4
Rintaro Hasegawa <rintaro.hasegawa@gmail.com>
2021-05-27 12:55:56 +0900

    docs: add text

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,21 @@ import Layout from './Layout';
 export default function Home() {
   return (
     <Layout>
-      <div className="flex">
-        <div className="my-4 mx-auto font-bold text-green-900 tracking-widest text-xl">
+      <div className="flex flex-col">
+        <div className="my-4 mx-auto font-bold text-green-900 tracking-widest text-xl text-center">
           こんにちわ！新海農園です！
+        </div>
+        <div className="my-4 mx-auto font-bold text-green-900 tracking-widest text-xl text-center leading-loose">
+          2024年開園に向け準備を進めています
+          <br />
+          体験&参加型でゆったり自然と触れ合える
+          <br />
+          そんなフルーツ農園を目指しています
+          <br />
+          一緒に「ふるさと」を作りませんか？
+        </div>
+        <div>
+          農業体験メニュー
         </div>
       </div>
     </Layout>
