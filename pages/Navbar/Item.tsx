61c6084c9edd6969ca1e4d253d21ff51727a9ba1
Rintaro Hasegawa <rintaro.hasegawa@gmail.com>
2021-05-27 19:15:26 +0900

    update: add hover animation

--- pages/Navbar/Item.tsx
+++ pages/Navbar/Item.tsx
@@ -1,8 +1,8 @@
 export default function Item({ name, iconFile }) {
   return (
-    <div className="mx-2 my-8 flex flex-col items-center">
+    <div className="group mx-2 my-8 flex flex-col items-center">
       <img src={`/images/icons/${iconFile}`} alt="" className="w-8 h-8" />
-      <div className="text-green-900 font-sansRound mt-2 whitespace-nowrap">
+      <div className="px-2 py-0.5 rounded text-green-900 group-hover:text-white font-sansRound mt-2 whitespace-nowrap group-hover:bg-green-600">
         {name}
       </div>
     </div>
