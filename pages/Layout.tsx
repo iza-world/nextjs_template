489c0cf6d897a3c9c1af91fdae738ef8584f9738
Rintaro Hasegawa <rintaro.hasegawa@gmail.com>
2021-05-27 19:07:13 +0900

    update: update background color

--- pages/Layout.tsx
+++ pages/Layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }) {
   return (
-    <div className="bg-green-50">
+    <div className="bg-green-50" style={{ backgroundColor: '#EEF5F6' }}>
       {children}
     </div>
   );
