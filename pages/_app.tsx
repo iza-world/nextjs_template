1dda748412613e912756d9498a99d8627d0c2645
Rintaro Hasegawa <rintaro.hasegawa@gmail.com>
2021-05-27 19:06:55 +0900

    feature: add fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import 'tailwindcss/tailwind.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      sansRound: ['"M PLUS Rounded 1c"', ...defaultTheme.fontFamily.sans],
+    },
   },
   variants: {
     extend: {},
